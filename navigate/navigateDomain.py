478ed0b7a76029e0b5b8cb98f89ce59135527490
devflash101 <devflash101@gmail.com>
2024-03-10 03:01:50 +0900

    click anchor text

--- navigate/navigateDomain.py
+++ navigate/navigateDomain.py
@@ -16,18 +16,40 @@
 #   check phone number
 #   check captcha
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+anchor_words = ['Demo', 'Call', 'Book', 'Schedule', 'Consultation', 'Consult', 'Appointment', 'Get Started', 'Start', 'Inquire', 'Learn', 'Discover', 'More Info', 'Find Out', 'Get a Quote', 'Talk',
+                'Explore', 'Details', 'Request', 'Connect', 'Get in', 'Contact']
 
 def navigate_domain(url):
     chrome_options = webdriver.ChromeOptions()
     #chrome_options.page_load_strategy = self.load_mode
     driver = webdriver.Chrome(options = chrome_options)
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-running-insecure-content')   
         
     driver.get(url)
-
-    name_field = driver.find_element_by_name('firstname')
+  
+    # contact_link = driver.find_element(By.XPATH, "//a[.//span[contains(text(), 'Contact')]]")
+    # contact_link.click()
+    
+    name_field = driver.find_element(By.NAME, 'firstname')
     if name_field:
+        name_field.send_keys('John Doe')
         print('success')
+    while True:
+        pass
     driver.quit()
 
 
-navigate_domain('http://parkstreet.com/')
+navigate_domain('https://chassi.com/contact/')
+# navigate_domain('https://parkstreet.com/')
+
+'''
+firstname
+lastname
+company
+phone
+email
+'''
