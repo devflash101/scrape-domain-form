2b35ab4ade5808610895a0f124b5c6a343ca3623
devflash101 <devflash101@gmail.com>
2024-03-19 08:48:11 +0900

    inline captcha

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ domain_list = ReadDomains('domains.csv')
 for domain in domain_list:
     # print(domain)
     domain_url = 'https://' + domain + '/'
+    print(domain_url)
     NavigateDomain(domain_url)
     # print(domain_url)
